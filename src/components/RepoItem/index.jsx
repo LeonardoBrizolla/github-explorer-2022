0875067dcaf649c34388b7499c99d5cdb0161e8d
leonardobrizolla <leonardobrizollars@gmail.com>
2022-01-12 23:04:44 -0300

    List repos

--- src/components/RepoItem/index.jsx
+++ src/components/RepoItem/index.jsx
@@ -2,8 +2,8 @@ export const RepoItem = (props) => {
   return (
     <li>
       <strong>{props.repo.name}</strong>
-      <p>{props.repo.desc}</p>
-      <a href={props.repo.link}>Acessar repo</a>
+      <p>{props.repo.description}</p>
+      <a href={props.repo.html_url}>Acessar repo</a>
     </li>
   );
 };
--- src/components/RepoList/index.jsx
+++ src/components/RepoList/index.jsx
@@ -2,14 +2,6 @@ import { useState, useEffect } from 'react';
 import './style.scss';
 import { RepoItem } from '../RepoItem';
 
-// https://api.github.com/users/LeonardoBrizolla/repos
-
-const repo = {
-  name: 'Unform',
-  desc: 'Forms in React',
-  link: 'https://github.com/unform/unform',
-};
-
 export const RepoList = () => {
   const [repos, setRepos] = useState([]);
 
@@ -24,10 +16,9 @@ export const RepoList = () => {
       <h1>Lista de Repos</h1>
 
       <ul>
-        <RepoItem repo={repo} />
-        <RepoItem repo={repo} />
-        <RepoItem repo={repo} />
-        <RepoItem repo={repo} />
+        {repos.map((repo) => (
+          <RepoItem key={repo.name} repo={repo} />
+        ))}
       </ul>
     </section>
   );
